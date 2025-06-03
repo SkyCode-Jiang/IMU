01b0a58d49a69f584731601fa56d2c586fa670fd
JCLStill <1737062013@qq.com>
2025-06-03 15:52:17 +0800

    Signed-off-by: JCLStill <1737062013@qq.com>

---
+++ ICM20948/My_ICM20948/My_ICM20948/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'My_ICM20948' 
+ * Target:  'My_ICM20948' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
